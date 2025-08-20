e8f2326d4f477516bc90b5659e69377554679648
Cameron Hightower <144738448+SimbaBuilds@users.noreply.github.com>
2025-08-20 12:36:39 -0500

    Update page.tsx

--- app/(protected)/repository/page.tsx
+++ app/(protected)/repository/page.tsx
@@ -257,7 +257,7 @@ export default function RepositoryPage() {
           </div>
           <div>
             <p className="text-sm text-blue-800 dark:text-blue-200">
-            <span className="font-medium">Tips:</span> - Use tags to improve retrieval (e.g. add the "email" tag to a resource to make sure Juniper has access to it every time it does something with email). - “Memories” are more likely to come up in casual conversation while “References”, “Samples”,  and “Notes” can be assigned to specific workflows.
+            <span className="font-medium">Tips:</span> <br />1. Use tags to improve retrieval.  For example: add the Email tag to a resource to make sure Juniper has access to it every time it does something with email.<br />2. Memories are more likely to come up in casual conversation while “References”, “Samples”,  and “Notes” can be assigned to specific workflows.
             </p>
           </div>
         </div>
